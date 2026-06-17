77ded571531df139037078e500d598e6c9c3146a
locqust <locqust@users.noreply.github.com>
2026-06-17 21:18:51 +0100

    Fix a data leak of privacy in identity bar

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ from routes.parental import parental_bp
 from routes.shortcuts import shortcuts_bp
 
 # Application version
-__version__ = "0.9.5.0-beta"
+__version__ = "0.9.5.1-beta"
 
 app = Flask(__name__)
 Compress(app)
